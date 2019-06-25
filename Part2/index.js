146c0ecb3c73ebe751900bb745b049923c9cf8f9
Garrett Woodall <garrett1woodall@gmail.com>
2019-06-24 21:32:02 -0500

    done

---
+++ Part2/index.js
@@ -0,0 +1,16 @@
+console.log("test")
+let idInput = document.getElementById("idInput");
+let colorInput = document.getElementById("colorInput")
+function setCard (){
+    let card = document.getElementById(idInput.value);
+    card.style.color = colorInput.value
+    console.log(card)
+}
+function reset (){
+  let j =  document.getElementsByClassName("myForm")
+      for (let i=0;i < j.length;i++){
+      
+      }
+  }
+
+
